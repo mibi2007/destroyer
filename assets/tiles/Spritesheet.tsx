594cc48f65855cb09538f4e46c4aea9ba6e2a676
mibi <binhhm2009@gmail.com>
2024-02-19 04:35:12 +0700

    Fix bug assets

--- assets/tiles/Spritesheet.tsx
+++ assets/tiles/Spritesheet.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Spritesheet" tilewidth="32" tileheight="32" tilecount="32" columns="8">
- <image source="../../assets/tiles/Spritesheet.png" width="256" height="128"/>
+ <image source="Spritesheet.png" width="256" height="128"/>
  <wangsets>
   <wangset name="Unnamed Set" type="mixed" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
